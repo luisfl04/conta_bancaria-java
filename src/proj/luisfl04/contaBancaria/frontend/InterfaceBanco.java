8949b84a090ed2983aefb8bf4cf3f85ff5bfcab5
luis felipe <luisfelipeconato08@gmail.com>
2024-05-06 19:53:09 -0300

    feat: Adição de inserção do numero de agencia da conta(Com tramento de erro) em InterfaceBanco,java

--- src/proj/luisfl04/contaBancaria/frontend/InterfaceBanco.java
+++ src/proj/luisfl04/contaBancaria/frontend/InterfaceBanco.java
@@ -20,6 +20,7 @@ public class InterfaceBanco {
         // Pedindo numero de conta:
         System.out.println("\nAgora, crie um número para sua conta(ele precisa ter 4 digitos):");
         ContaBancaria.numero_da_conta = scanf.nextInt();
+        
         // Fazendo tratamento de erro caso o usuário digite um valor maior do que 4 digitos:
         // Fiz essa verificação tranformando o numero em uma string e contando os caracters.
         String string_numero_da_conta = Integer.toString(ContaBancaria.numero_da_conta);
@@ -28,13 +29,36 @@ public class InterfaceBanco {
         // Enquanto o tamanho do numero for diferente de 4, é pedido novamente.
         while(quantidade_de_caracteres_numero_da_conta != 4){
             // Exibindo mensagem de erro e pedindo novamente.
-            System.out.println("\nO valor que voce inseriu é inválido! insira novamente abaixo:");
+            System.out.println("\nA quantidade de número que você inseriu é inválida! Insira novamente abaixo:");
             ContaBancaria.numero_da_conta = scanf.nextInt();
+            
             // É preciso tranformar o numero em string e verificar o tamanho novamente para a lógica ser válida.
             String numero_da_conta_no_wilhe = Integer.toString(ContaBancaria.numero_da_conta);
             int quantidade_de_caracteres_numero_da_conta_no_wilhe = numero_da_conta_no_wilhe.length();
             quantidade_de_caracteres_numero_da_conta = quantidade_de_caracteres_numero_da_conta_no_wilhe;
         }
+
+        // pedindo a agencia da conta:
+        System.out.println("\nCrie agora uma agencia para você(Com 3 digitos):");
+        ContaBancaria.agencia_da_conta = scanf.nextInt();
+
+        // fazendo tratamento de erro em relação a quantidade de caracteres:
+        String string_agencia_da_conta = Integer.toString(ContaBancaria.agencia_da_conta);
+        int quantidade_de_caracteres_agencia_da_conta = string_agencia_da_conta.length();
+        
+        // Enquanto não for válido, peço novamente:
+        while(quantidade_de_caracteres_agencia_da_conta != 3) {
+            // Exibindo mensagem de erro:
+            System.out.println("\nA quantidade de numeros que você inseriu é inválida! Insira novamente abaixo: ");
+            ContaBancaria.agencia_da_conta = scanf.nextInt();
+
+            // Fazendo a mesma lógica para conseguir validar os dados:
+            String string_agencia_da_conta_no_while = Integer.toString(ContaBancaria.agencia_da_conta);
+            int quantidade_de_caracteres_agencia_da_conta_no_while = string_agencia_da_conta_no_while.length();
+            quantidade_de_caracteres_agencia_da_conta = quantidade_de_caracteres_agencia_da_conta_no_while;
+        }
+
+        
 
 
 
